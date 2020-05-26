4eaf68fbecfbf5e8dfb9c91c1e8a6cfc07ff1114
Luke <luke@cloudthrifty.com>
2020-05-25 22:08:47 -0400

    fix screenlib

---
+++ reading/reading.go
@@ -0,0 +1,26 @@
+package gouiscreen
+
+import (
+	"gihthub.com/goog-lukemc/gouielement"
+)
+
+func Reader(content *goReadingData, comp *gouielement.ElementLib, parent string) error {
+	if content == nil {
+		content = getDefaultContent(parent, v)
+	}
+	
+
+}
+
+func getDefaultContent(parent string, comp *gouielement.ElementLib) *gouielement.ReadingData {
+	c := &gouielement.ReadingData{
+		Title:    "Welcome to the Go UI framework using Webassembly",
+		Subtitle: "Calm down!, with project is a WIP",
+	}
+	t1 := "Welcome to the goui framework. Have fun!"
+	c.Content = append(c.Content,
+		comp.Span(parent,t1)
+	)
+	
+	return c
+}
