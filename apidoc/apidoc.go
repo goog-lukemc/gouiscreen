2675c10a23341c50fd4a77c4a0996c8899b42083
Luke <luke@cloudthrifty.com>
2020-05-25 22:26:22 -0400

    commit out old code

--- apidoc/apidoc.go
+++ apidoc/apidoc.go
@@ -1,69 +1,69 @@
-package apidoc
+package gouiscreen
 
-import (
-	"gouicomponent"
-	"gouidom"
-)
+// import (
+// 	"gouicomponent"
+// 	"gouidom"
+// )
 
-type Doc struct {
-	Name    string
-	Content *Article
-	Demo    *HowTo
-}
+// type Doc struct {
+// 	Name    string
+// 	Content *Article
+// 	Demo    *HowTo
+// }
 
-type Article struct {
-	Heading    string
-	SubHeading string
-	Sections   []*Section
-}
+// type Article struct {
+// 	Heading    string
+// 	SubHeading string
+// 	Sections   []*Section
+// }
 
-type Section struct {
-	Heading    string
-	Paragraphs []string
-}
+// type Section struct {
+// 	Heading    string
+// 	Paragraphs []string
+// }
 
-type HowTo struct {
-	Method       string
-	RequestBody  map[string]interface{}
-	ResponseBody map[string]interface{}
-	Path         string
-	Example      string
-}
+// type HowTo struct {
+// 	Method       string
+// 	RequestBody  map[string]interface{}
+// 	ResponseBody map[string]interface{}
+// 	Path         string
+// 	Example      string
+// }
 
-func NewAPIDoc(v *gouidom.VDOM, d []*Doc) []*gouidom.Element {
+// func NewAPIDoc(v *gouidom.VDOM, d []*Doc) []*gouidom.Element {
 
-	// Create a new component lib
-	comp := gouicomponent.NewComponentLib(v)
+// 	// Create a new component lib
+// 	comp := gouicomponent.NewComponentLib(v)
 
-	// Create our main content with 3 divs
-	content := comp.NewGrid("html/body", 3, "container", "grid")
+// 	// Create our main content with 3 divs
+// 	content := comp.NewGrid("html/body", 3, "container", "grid")
 
-	_ = content
+// 	_ = content
 
-	names := []string{}
-	for _, doc := range d {
-		names = append(names, doc.Name)
-	}
+// 	names := []string{}
+// 	for _, doc := range d {
+// 		names = append(names, doc.Name)
+// 	}
 
-	comp.NewButtonGroup(gouicomponent.PathOf(content[0]), "Routes", names...)
+// 	comp.NewButtonGroup(gouicomponent.PathOf(content[0]), "Routes", names...)
 
-	// Lets build a document
-	c1 := comp.NewArticle(gouicomponent.PathOf(content[1]), "article-outer")
+// 	// Lets build a document
+// 	c1 := comp.NewArticle(gouicomponent.PathOf(content[1]), "article-outer")
 
-	for _, doc := range d {
-		comp.NewHeading(gouicomponent.PathOf(c1), doc.Content.Heading, "1", "heading-main")
-		for _, sec := range doc.Content.Sections {
-			s := comp.NewSection(gouicomponent.PathOf(c1), "section-inner")
-			comp.NewHeading(gouicomponent.PathOf(s), sec.Heading, "3", "heading-section")
-			for _, para := range sec.Paragraphs {
-				comp.NewParagraph(gouicomponent.PathOf(s), para, "para-text")
-			}
-		}
-		r1 := comp.NewArticle(gouicomponent.PathOf(content[2]), "article-example")
-		s1 := comp.NewSection(gouicomponent.PathOf(r1))
-		comp.NewPreCode(gouicomponent.PathOf(s1), doc.Demo.Example)
-	}
+// 	for _, doc := range d {
+// 		comp.NewHeading(gouicomponent.PathOf(c1), doc.Content.Heading, "1", "heading-main")
+// 		for _, sec := range doc.Content.Sections {
+// 			s := comp.NewSection(gouicomponent.PathOf(c1), "section-inner")
+// 			comp.NewHeading(gouicomponent.PathOf(s), sec.Heading, "3", "heading-section")
+// 			for _, para := range sec.Paragraphs {
+// 				comp.NewParagraph(gouicomponent.PathOf(s), para, "para-text")
+// 			}
+// 		}
+// 		r1 := comp.NewArticle(gouicomponent.PathOf(content[2]), "article-example")
+// 		s1 := comp.NewSection(gouicomponent.PathOf(r1))
+// 		comp.NewPreCode(gouicomponent.PathOf(s1), doc.Demo.Example)
+// 	}
 
-	return content
+// 	return content
 
-}
+// }
--- codereview/gofilefmt.go
+++ codereview/gofilefmt.go
@@ -1,113 +1,113 @@
-package codereview
+package gouiscreen
 
-import (
-	"bufio"
-	"fmt"
-	"gouicomponent"
-	"gouidom"
-	"io"
-	"path"
-	"strconv"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"gouicomponent"
+// 	"gouidom"
+// 	"io"
+// 	"path"
+// 	"strconv"
+// )
 
-func GoFMTWeb(parent string, file io.Reader, v *gouidom.VDOM) {
-	// Create a new component lib
-	comp := gouicomponent.NewComponentLib(v)
+// func GoFMTWeb(parent string, file io.Reader, v *gouidom.VDOM) {
+// 	// Create a new component lib
+// 	comp := gouicomponent.NewComponentLib(v)
 
-	// Create a go formater
-	//var parser wasmGoFMT
+// 	// Create a go formater
+// 	//var parser wasmGoFMT
 
-	container := comp.NewDivWrapper(parent)
+// 	container := comp.NewDivWrapper(parent)
 
-	// Create a wrapper for the item
-	//container := gouicomponent.NewWrapper(parent, v)
+// 	// Create a wrapper for the item
+// 	//container := gouicomponent.NewWrapper(parent, v)
 
-	lineScanner := bufio.NewScanner(file)
+// 	lineScanner := bufio.NewScanner(file)
 
-	// Split the file on lines
-	lineScanner.Split(bufio.ScanLines)
+// 	// Split the file on lines
+// 	lineScanner.Split(bufio.ScanLines)
 
-	// Loop and generate the document
-	l := 1
-	for lineScanner.Scan() {
+// 	// Loop and generate the document
+// 	l := 1
+// 	for lineScanner.Scan() {
 
-		// Line number parent div
-		parentSpan := comp.NewDiv(path.Join(container.Parent, container.ID), map[string]string{"data-num": fmt.Sprintf("%d", l)}, "golang", "line")
+// 		// Line number parent div
+// 		parentSpan := comp.NewDiv(path.Join(container.Parent, container.ID), map[string]string{"data-num": fmt.Sprintf("%d", l)}, "golang", "line")
 
-		// Line Span
-		lineToolBar := comp.NewSpan(path.Join(parentSpan.Parent, parentSpan.ID), "", "golang", "line-toolbar")
+// 		// Line Span
+// 		lineToolBar := comp.NewSpan(path.Join(parentSpan.Parent, parentSpan.ID), "", "golang", "line-toolbar")
 
-		// Line Span
-		_ = comp.NewSpan(path.Join(lineToolBar.Parent, lineToolBar.ID), fmt.Sprintf("%d", l), "golang", "line-num")
+// 		// Line Span
+// 		_ = comp.NewSpan(path.Join(lineToolBar.Parent, lineToolBar.ID), fmt.Sprintf("%d", l), "golang", "line-num")
 
-		// Get the text from the reader
-		text := lineScanner.Text()
+// 		// Get the text from the reader
+// 		text := lineScanner.Text()
 
-		// Get the line results and loop to build the spans
-		codeScan := getCodeTokens("unknown", []byte(text))
+// 		// Get the line results and loop to build the spans
+// 		codeScan := getCodeTokens("unknown", []byte(text))
 
-		// Loop the code scan line and set classes
-		for pos, lr := range codeScan {
+// 		// Loop the code scan line and set classes
+// 		for pos, lr := range codeScan {
 
-			if lr.value == "" {
-				lr.value = "->"
-			}
+// 			if lr.value == "" {
+// 				lr.value = "->"
+// 			}
 
-			_ = comp.NewSpan(path.Join(parentSpan.Parent, parentSpan.ID), lr.value, getSegmentClass(lr, codeScan, pos)...)
+// 			_ = comp.NewSpan(path.Join(parentSpan.Parent, parentSpan.ID), lr.value, getSegmentClass(lr, codeScan, pos)...)
 
-		}
+// 		}
 
-		l++
-	}
-}
+// 		l++
+// 	}
+// }
 
-func getSegmentClass(seg *codeDetail, res []*codeDetail, pos int) []string {
-	c := []string{"line-segment"}
+// func getSegmentClass(seg *codeDetail, res []*codeDetail, pos int) []string {
+// 	c := []string{"line-segment"}
 
-	switch {
-	case seg.isKeyWord:
-		c = append(c, "keyword")
-	case seg.name == "STRING":
-		c = append(c, "text-string")
-	case seg.isExported && seg.name == "IDENT":
+// 	switch {
+// 	case seg.isKeyWord:
+// 		c = append(c, "keyword")
+// 	case seg.name == "STRING":
+// 		c = append(c, "text-string")
+// 	case seg.isExported && seg.name == "IDENT":
 
-		if pos != 0 {
-			if res[pos-1].lit == "." {
-				if res[pos+1].lit == "(" {
-					c = append(c, "function")
-				} else {
-					c = append(c, "property")
-				}
-			}
-		}
-	case seg.name == "COMMENT":
-		c = append(c, "comment")
-	}
+// 		if pos != 0 {
+// 			if res[pos-1].lit == "." {
+// 				if res[pos+1].lit == "(" {
+// 					c = append(c, "function")
+// 				} else {
+// 					c = append(c, "property")
+// 				}
+// 			}
+// 		}
+// 	case seg.name == "COMMENT":
+// 		c = append(c, "comment")
+// 	}
 
-	return c
-}
+// 	return c
+// }
 
-// HighlightLines raises the code line to the users attention and returns the center vertical position
-// of the highlighted block.
-func HighlightLines(lines []string) string {
-	for pos, lineNumber := range lines {
-		ele, err := gouidom.GetElementByID(lineNumber)
-		if err != nil {
-			gouidom.CLog("%s", err.Error())
-		}
-		if pos == 0 {
-			ele.ScrollIntoView(true)
-			ele.AddClass("top-shadow")
-		}
-		ele.AddClass("highlight")
-		if pos == len(lines)-1 {
-			ele.AddClass("bottom-shadow")
-		}
-	}
-	return strconv.Itoa(len(lines) / 2)
-}
+// // HighlightLines raises the code line to the users attention and returns the center vertical position
+// // of the highlighted block.
+// func HighlightLines(lines []string) string {
+// 	for pos, lineNumber := range lines {
+// 		ele, err := gouidom.GetElementByID(lineNumber)
+// 		if err != nil {
+// 			gouidom.CLog("%s", err.Error())
+// 		}
+// 		if pos == 0 {
+// 			ele.ScrollIntoView(true)
+// 			ele.AddClass("top-shadow")
+// 		}
+// 		ele.AddClass("highlight")
+// 		if pos == len(lines)-1 {
+// 			ele.AddClass("bottom-shadow")
+// 		}
+// 	}
+// 	return strconv.Itoa(len(lines) / 2)
+// }
 
-func ScrollPositionCenter(center string) {
-	// Get the window size with window
+// func ScrollPositionCenter(center string) {
+// 	// Get the window size with window
 
-}
+// }
