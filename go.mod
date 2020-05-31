a0b78d9047c16e7794c716f84a8f8511b8b0a884
Luke <luke@cloudthrifty.com>
2020-05-31 18:08:32 -0400

    added works function

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/goog-lukemc/gouiscreen
 
 go 1.13
+
+require (
+	github.com/goog-lukemc/gouidom v0.0.11
+	github.com/goog-lukemc/gouielement v0.0.22
+)
--- reading.go
+++ reading.go
@@ -1,27 +1,59 @@
 package gouiscreen
 
 import (
-	"gihthub.com/goog-lukemc/gouielement"
+	"github.com/goog-lukemc/gouidom"
+	"github.com/goog-lukemc/gouielement"
 )
 
+type WorksData struct {
+	Title    string
+	Subtitle string
+	Articles []*gouielement.ArticleData
+}
+
 // Reader create a readable content
-func Reader(content *gouielement.ReadingData, comp *gouielement.ElementLib, parent string) error {
-	if content == nil {
-		content = getDefaultContent(parent, v)
+func DocReader(parent string, comp *gouielement.ElementLib, works *WorksData) error {
+	if works == nil {
+		works = append(works, getDefaultContent(parent, v))
 	}
+
+	// Create a body centering div
+	cd := comp.Div(parent, map[string]string{})
+
+	// Create the left side bar which create a left had list of the documents in the collection
+	// TODO: Implement
+	comp.Div(gouidom.PathOf(cd), map[string]string{})
+
+	for _, item := range *content {
+
+	}
+	// Content Body
 	comp.Readable(content)
+
+	// Create a right hand index of the sub headings in the document
+	// TODO: Implement
+	comp.Div(gouidom.PathOf(cd), map[string]string{})
 
 }
 
-func getDefaultContent(parent string, comp *gouielement.ElementLib) *gouielement.ReadingData {
-	c := &gouielement.ReadingData{
-		Title:    "Welcome to the Go UI framework using Webassembly",
-		Subtitle: "Calm down!, with project is a WIP",
+func getDefaultContent(parent string, comp *gouielement.ElementLib) *WorksData {
+	w := &WorksData{
+		Title: "Something New!"
+		Subtitle: "My Wife Made me do it."
+		Articles: []&gouielement.ArticlesData{
+			Title:"Welcome to the Go UI framework using Webassembly",
+			Subtitle:"And Here we are!"
+			Content:
+				&gouielement.ContentCFG{
+					Typ:gouidom.HTMLTag.Span,
+					CFG:map[string]interface{}{
+						"text":"The guiding principal of this project is to take a subset of the existing HTML, and javascript specifications and port them to gos webassembly implementation. From these building block UI can be represented in idiomatic go code without the need for markup."
+					},
+				}
+			},
+		},
 	}
-	t1 := "Welcome to the goui framework. Have fun!"
-	c.Content = append(c.Content,
-		comp.Span(parent,t1)
-	)
+	
 
-	return c
+	return w
 }
